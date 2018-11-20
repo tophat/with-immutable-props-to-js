930f81f4342dfd47c7eaf2035fb2cdc3d4965c68
Michael Rose <michael.rose@tophatmonocle.com>
2018-11-20 15:09:43 -0500

    Lol react-dom too

--- tests/setupTestFramework.js
+++ tests/setupTestFramework.js
@@ -1,11 +1,10 @@
 import enzyme from 'enzyme'
-import React15Adapter from 'enzyme-adapter-react-15'
-import React16Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
 const [reactMajorVersion] = React.version.split('.')
 
-const adapter =
-    reactMajorVersion === '15' ? new React15Adapter() : new React16Adapter()
+const Adapter = require(reactMajorVersion === '15'
+    ? 'enzyme-adapter-react-15'
+    : 'enzyme-adapter-react-16')
 
-enzyme.configure({ adapter })
+enzyme.configure({ adapter: new Adapter() })
