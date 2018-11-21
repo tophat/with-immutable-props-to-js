d5c4c47e1941a0c33f226596e448dd86fa6719ba
Michael Rose <michael.rose@tophatmonocle.com>
2018-11-21 12:33:04 -0500

    Update eslint config with more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,14 @@ module.exports = {
         },
     },
     env: {
-        jest: true
+        jest: true,
     },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
+        'plugin:jest/recommended',
         'prettier',
+        'prettier/react',
     ],
     plugins: [
         'prettier',
@@ -26,10 +28,27 @@ module.exports = {
             trailingComma: 'all',
             singleQuote: true,
         }],
+        camelcase: ['error', { properties: 'never' }],
+        curly: ['error', 'all'],
+        'dot-notation': 'error',
+        eqeqeq: 'error',
+        'no-duplicate-imports': 'error',
+        'no-nested-ternary': 'error',
+        'no-useless-computed-key': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'prefer-template': 'error',
+        'jest/prefer-to-be-null': 'error',
+        'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/prefer-to-contain': 'error',
+        'react/default-props-match-prop-types': 'error',
+        'react/require-default-props': 'error',
+        'react/no-redundant-should-component-update': 'error',
     },
     settings: {
         react: {
-            version: '15.0',
+            version: '16.0',
         },
     },
 }
