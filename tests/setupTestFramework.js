6cd12b9758c39fdba68ac2081b7e2bb9f0e9ce17
Michael Rose <michael_rose@outlook.com>
2018-11-21 10:58:37 -0500

    Matrix builds for react and immutable (#11)

--- tests/setupTestFramework.js
+++ tests/setupTestFramework.js
@@ -1,4 +1,10 @@
 import enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+
+const [reactMajorVersion] = React.version.split('.')
+
+const Adapter = require(reactMajorVersion === '15'
+    ? 'enzyme-adapter-react-15'
+    : 'enzyme-adapter-react-16')
 
 enzyme.configure({ adapter: new Adapter() })
