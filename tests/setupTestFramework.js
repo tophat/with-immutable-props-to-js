1ec3d29c7ad31fcdb705eca82dcc75d57d97d6d6
Michael Rose <michael.rose@tophatmonocle.com>
2019-08-16 16:13:34 -0400

    build: Drop support for React 15 (#77)

--- tests/setupTestFramework.js
+++ tests/setupTestFramework.js
@@ -1,10 +1,5 @@
 import enzyme from 'enzyme'
-import React from 'react'
 
-const [reactMajorVersion] = React.version.split('.')
-
-const Adapter = require(reactMajorVersion === '15'
-    ? 'enzyme-adapter-react-15'
-    : 'enzyme-adapter-react-16')
+const Adapter = require('enzyme-adapter-react-16')
 
 enzyme.configure({ adapter: new Adapter() })
